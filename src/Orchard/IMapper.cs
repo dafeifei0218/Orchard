b8d2ceafbdd9f648d34ba41e4f6872b4b46c99b4
dafeifei0218 <ytr19860218@163.com>
2016-06-06 23:45:22 +0800

    仓储注释

--- src/Orchard/IMapper.cs
+++ src/Orchard/IMapper.cs
@@ -5,6 +5,11 @@ namespace Orchard {
     /// <typeparam name="TSource">源</typeparam>
     /// <typeparam name="TTarget">目标</typeparam>
     public interface IMapper<TSource, TTarget> : IDependency {
+        /// <summary>
+        /// 映射
+        /// </summary>
+        /// <param name="source">源</param>
+        /// <returns></returns>
         TTarget Map(TSource source);
     }
 }
