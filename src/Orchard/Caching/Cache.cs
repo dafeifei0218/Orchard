ede210f946574aeaca4709616dd149929b96be1a
dafeifei0218 <ytr19860218@163.com>
2017-03-03 11:03:00 +0800

    修改缓存类

--- src/Orchard/Caching/Cache.cs
+++ src/Orchard/Caching/Cache.cs
@@ -33,10 +33,10 @@ namespace Orchard.Caching {
         public TResult Get(TKey key, Func<AcquireContext<TKey>, TResult> acquire) {
             var entry = _entries.AddOrUpdate(key,
                 // "Add" lambda
-                // 添加lambda
+                // 添加Lambda
                 k => AddEntry(k, acquire),
                 // "Update" lambda
-                // 更新lambda
+                // 更新Lambda
                 (k, currentEntry) => UpdateEntry(currentEntry, k, acquire));
 
             return entry.Result;
